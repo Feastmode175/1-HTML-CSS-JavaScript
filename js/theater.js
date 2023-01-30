961d7a3018d2623e13391b14fd483582f9dec2b1
Karl Feaster <feaster11c@gmail.com>
2023-01-30 08:47:53 -0500

    Added button and buyTicket func

---
+++ js/theater.js
@@ -0,0 +1,13 @@
+const GENERAL_ADMISSION_COST = 20;
+const DISCOUNT_ADMISSION_COST = 10;
+
+function buyTicket () {
+  const age = prompt('What is your age?')
+  let cost;
+  if ((age<=12) || (age>=65)) {
+    cost = DISCOUNT_ADMISSION_COST;
+  } else {
+    cost = GENERAL_ADMISSION_COST;
+  }
+  alert('Your ticket will cost: $' + cost +'.');
+}
