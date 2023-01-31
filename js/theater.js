51e87c5ed08b0bedd76b0ad5b5721a307d1fb2aa
Karl Feaster <feaster11c@gmail.com>
2023-01-30 22:33:01 -0500

    added matinee discount func

--- js/theater.js
+++ js/theater.js
@@ -2,17 +2,14 @@ const GENERAL_ADMISSION_COST = 20;
 const DISCOUNT_ADMISSION_COST = 10;
 const MATINEE_DISCOUNT = 3;
 
-function buyTicket () {
+function buyTicket() {
   const age = prompt('What is your age?');
   let cost = getBaseTicketCost(age);
+  let isMatinee;
+  cost = applyMatineeDiscount(cost,isMatinee);
   
-  const isMatinee = prompt('Are you attending a matinee show?');
-    if((isMatinee === 'yes') || (isMatinee === 'y')) {
-      cost = cost - MATINEE_DISCOUNT;
-    }
   alert('Your ticket will cost: $' + cost +'.');
 }
-
 
 function getBaseTicketCost(age) {
   if ((age<=12) || (age>=65)) {
@@ -20,3 +17,11 @@ function getBaseTicketCost(age) {
   }
   return GENERAL_ADMISSION_COST
 }
+
+function applyMatineeDiscount(cost,isMatinee) {
+  isMatinee = prompt('Are you attending a matinee show?');
+  if ((isMatinee.toLowerCase() === 'yes') || (isMatinee.toLowerCase() === 'y')) {
+    return cost - MATINEE_DISCOUNT;
+    }
+  return cost;
+  }
