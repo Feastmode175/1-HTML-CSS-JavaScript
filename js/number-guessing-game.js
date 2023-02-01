d8db93374a6deb78bb0e09192af5ead4d4ee5bb6
Karl Feaster <feaster11c@gmail.com>
2023-01-31 19:26:42 -0500

    added "target" generation code

---
+++ js/number-guessing-game.js
@@ -0,0 +1,9 @@
+function runGame() {
+
+  const randomNumber = Math.random() * 100;
+  const randomInteger = Math.floor(randomNumber);
+  const target = randomInteger + 1;
+
+  //const target = Math.floor(Math.random() *100) + 1;
+  
+}
