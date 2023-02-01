634ef7bb8abebbd270d45ed78937caecd07f4e48
Karl Feaster <feaster11c@gmail.com>
2023-01-31 20:01:10 -0500

    added guess string prompt and checkGuess func

--- js/number-guessing-game.js
+++ js/number-guessing-game.js
@@ -1,9 +1,17 @@
 function runGame() {
 
+  let guessString = '';
+  let guessNumber = 0;
+  let correct = false;
   const randomNumber = Math.random() * 100;
   const randomInteger = Math.floor(randomNumber);
   const target = randomInteger + 1;
 
   //const target = Math.floor(Math.random() *100) + 1;
   
+  do {
+    guessString = prompt('I am thinking of a number in the range 1 to 100. \n\n')
+    guessNumber = +guessString;
+    correct = checkGuess(guessNumber, target);
+  } while (!correct);
 }
