481bfcf49bfd7a72abc275fa99f8e9459e144088
Karl Feaster <feaster11c@gmail.com>
2023-02-11 10:35:31 -0500

    added js logic to generate left side images

---
+++ workshop-4/matching-game.js
@@ -0,0 +1,17 @@
+let numberOfFaces = 5;
+const leftSide = document.querySelector('#leftSide');
+
+function generateFaces () {
+  for (i = 0; i < 5; i++) {
+    let face = document.createElement('img');
+    face.src = '../images/smile.png';
+    
+    let randomTop = Math.floor(Math.random() * 400 ) + 1;
+    let randomLeft = Math.floor(Math.random() * 400 ) + 1;
+
+    face.style.top = randomTop + 'px';
+    face.style.left = randomLeft + 'px';
+
+    leftSide.appendChild(face);
+  }
+}
